a5912759baeecf441dfc391b544841c1216eb15d
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-29 10:59:37 -0800

    update.components::users,App,js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-// import React, { useState } from "react";
-// import AddUser from "./components/Users/AddUser";
-// import UsersList from "./components/Users/UsersList";
 
-// function App() {
-//   const [userList, SetUserList] = useState([]);
-
-//   const addUserHandler = (uName, uAge) => {
-//     SetUserList((prevState) =>{ return [
-//       ...prevState,
-//       { name: uName, age: uAge, id: Math.random().toString() },
-//     ]});
-//   };
-//   return (
-//     <div>
-//       <AddUser onAddUser={addUserHandler} />
-//       <UsersList users={[userList]} />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React, { useState } from 'react';
 
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,21 +1,3 @@
-// import Styles from "./UsersList.module.css";
-// import Card from "../UI/Card";
-// const UsersList = (props) => {
-//   return (
-//     <Card className={Styles.users}>
-//     <ul>
-
-//       {props.users.map((user) => (
-//         <li key={user.id}>
-//           {user.name} ({user.age} Years Old)
-//         </li>
-//       ))}
-//     </ul>
-//     </Card>
-//   );
-// };
-
-// export default UsersList;
 
 import React from 'react';
 
