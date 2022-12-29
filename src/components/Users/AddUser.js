b956ee0a4e35a8d6d93aabd9c292be14de6cfda5
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-28 16:46:09 -0800

    CardComponent.add::for reuseable as  a wrapper

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,20 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const AddUserHandler = (event) => {
     event.preventDEfault();
   };
   return (
-    <form onSubmit={AddUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username"></input>
-      <label htmlFor="age">Age</label>
-      <input type="number" id="Age"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input  }>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username"></input>
+        <label htmlFor="age">Age</label>
+        <input type="number" id="Age"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
