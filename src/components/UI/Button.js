a6bd3befae8fa3cc0eabdd8588149b4872a56a48
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-28 23:37:10 -0800

    comlpeted.project

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,11 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button className={classes.button} type={props.type || "button"} onClick={props.onClick}>
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -62,12 +62,24 @@ const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
+  const [error, setError] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please Enter a valid name and age",
+      });
+
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid Age",
+        message: "Please Enter valid age (>0)",
+      });
+
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -83,9 +95,19 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          onConfirm={errorHandler}
+          title={error.title}
+          message={error.message}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
